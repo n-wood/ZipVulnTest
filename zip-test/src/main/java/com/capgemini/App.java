3338938fd4758c82e6681f12c0769931957ac2c3
Nathan Wood <nathan.wood@digital.hmrc.gov.uk>
2018-08-28 11:59:39 +0100

    added zip file to project

--- zip-test/src/main/java/com/capgemini/App.java
+++ zip-test/src/main/java/com/capgemini/App.java
@@ -8,13 +8,21 @@ public class App
 {
     public static void main( String[] args )
     {
+     String filename = "zip-slip.zip";
+     App example = new App();
+     example.unPackZipFile(filename);
      
-     String filename = "/Users/nathan/Applications/seccom/dodgyZip/zip-slip.zip";
-     File unzipLocation = new File("/Users/nathan/Applications/seccom/safeplace");
-     ZipUnArchiver unzip = new ZipUnArchiver(new File(filename));
-     unzip.extract(
-    		 "../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../tmp/evil.txt",
-    		 unzipLocation);
-     System.out.println("Done");
+    }
+    
+    private void unPackZipFile(String filename)
+    {
+    	File file = new File(this.getClass().getClassLoader().getResource(filename).getFile());
+    	   
+        File unzipLocation = new File("/tmp/safeplace");
+        ZipUnArchiver unzip = new ZipUnArchiver(file);
+        unzip.extract(
+       		 "../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../tmp/evil.txt",
+       		 unzipLocation);
+        System.out.println("Done");
     }
 }
